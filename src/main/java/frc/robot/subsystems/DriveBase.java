9be9a043c14bd057e0ac2d75b2f99c3482ff6c88
BakedPotatoLord <918780@dpsk12.net>
2022-05-26 19:46:59 -0600

    finished drivebase

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -11,12 +12,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveBase extends SubsystemBase {
 
   private static Spark m_fl = new Spark(2);
-  private static Spark m_fr = new Spark(2);
+  private static Spark m_fr = new Spark(1);
   private static Spark m_rl = new Spark(3);
-  private static Spark m_rr = new Spark(2);
+  private static Spark m_rr = new Spark(0);
 
+  
+
+  private MecanumDrive m_robotDrive;
   /** Creates a new ExampleSubsystem. */
-  public DriveBase() {}
+  public DriveBase() {
+
+    m_fr.setInverted(true);
+    m_rr.setInverted(true);
+
+    m_robotDrive = new MecanumDrive(m_fl, m_rl, m_fr, m_rr);
+  
+  }
+
+  public void driveCartesian(double y,double x,double z,double t){
+    m_robotDrive.driveCartesian(y, x, z,0.0);
+  }
 
 
 }
