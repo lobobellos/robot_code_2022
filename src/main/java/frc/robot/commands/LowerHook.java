e81ebe7901e7473654828dd4ff7b5b115577d0cb
BakedPotatoLord <918780@dpsk12.net>
2022-05-26 20:26:43 -0600

    added hook functs

---
+++ src/main/java/frc/robot/commands/LowerHook.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Climber;
+
+public class LowerHook extends InstantCommand{
+
+    public LowerHook(Climber climb){
+        super(()->
+      climb.lower()
+      ,climb);
+    }
+}
