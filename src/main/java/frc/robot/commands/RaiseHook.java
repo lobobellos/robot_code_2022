f45970a4df62a6304e646475c299a3a1c310a780
BakedPotatoLord <918780@dpsk12.net>
2022-05-26 20:20:15 -0600

    created raiseHook command

---
+++ src/main/java/frc/robot/commands/RaiseHook.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Climber;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+/** An example command that uses an example subsystem. */
+public class RaiseHook extends InstantCommand {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public RaiseHook(Climber climb) {
+    super(()->
+      climb.raise()
+      ,climb);
+
+  }
+
+
+}
