f86c5b604e3af7ef9eba78757aa3d30ae0ad116f
BakedPotatoLord <918780@dpsk12.net>
2022-05-26 20:15:50 -0600

    added climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems;
 
-public class Climber {
-    
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Climber extends SubsystemBase {
+    private Spark m_climb=new Spark(Constants.climberChannel);
+
+    public Climber(){
+
+    }
+
+    public void raise(){
+        m_climb.set(1);
+    }
+
+    public void lower(){
+        m_climb.set(-1);
+    }
+
+    public void stop(){
+        m_climb.stopMotor();
+    }
 }
