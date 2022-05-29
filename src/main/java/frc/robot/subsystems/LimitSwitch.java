8fa19d3562aa6f7ccb3c6d1581dccc9246209b08
BakedPotatoLord <918780@dpsk12.net>
2022-05-28 20:21:44 -0600

    created Limitswitch class

--- src/main/java/frc/robot/subsystems/LimitSwitch.java
+++ src/main/java/frc/robot/subsystems/LimitSwitch.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems;
 
-public class LimitSwitch {
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class LimitSwitch extends SubsystemBase {
+    DigitalInput limitSwitch = new DigitalInput(2);
+    Boolean lastState;
+
     
+
+    Boolean get(){
+        return limitSwitch.get();
+    }
+    Boolean getPressed(){
+        return (!lastState && limitSwitch.get());
+    }
+
+
+
+    @Override
+    public void periodic() {
+        lastState = limitSwitch.get();
+    }
+
+
 }
