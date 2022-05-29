745d911f10e6aa51cc2a3fddce89113517b8fd79
BakedPotatoLord <918780@dpsk12.net>
2022-05-28 20:32:54 -0600

    created shooter class

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -1,7 +1,26 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
-    
+    public Spark shooterM = new Spark(Constants.mainShooterChannel);
+    public Spark shooterT = new Spark(Constants.topShooterChannel);
+
+    void setVoltageM(double voltage){
+        shooterM.setVoltage(voltage);
+    }
+
+    void setVoltageT(double voltage){
+        shooterT.setVoltage(voltage);
+    }
+
+    void stopM(){
+        shooterM.stopMotor();
+    }
+
+    void stopT(){
+        shooterT.stopMotor();
+    }
 }
