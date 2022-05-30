c955e00e3cfeff31819402a5bf07685944392476
BakedPotatoLord <918780@dpsk12.net>
2022-05-30 11:09:47 -0600

    quicksave

---
+++ src/main/java/frc/robot/commands/StartIntake.java
@@ -0,0 +1,16 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LimitSwitch;
+
+public class StartIntake extends CommandBase {
+
+    private Intake intake;
+    private  LimitSwitch switch;
+
+    public StartIntake(Intake sucker  LimitSwitch switch ) {
+        intake = sucker;
+        lSwitch 
+    }
+}
