c781cce1248db7907d88e7e0402f48eaa259c669
BakedPotatoLord <918780@dpsk12.net>
2022-05-30 10:55:00 -0600

    created alignbase code

---
+++ src/main/java/frc/robot/commands/AlignBase.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Limelight;
+
+public class AlignBase extends CommandBase {
+    
+    private static DriveBase driveBase;
+    private static Limelight limelight;
+
+    public AlignBase(DriveBase base, Limelight light){
+        driveBase = base;
+        limelight = light;
+        addRequirements(driveBase,limelight);
+        
+    }
+    
+    @Override
+    public void initialize() {
+        
+    }
+
+    public void execute() {
+        if(limelight.tx > 1){
+            if(limelight.tx < 10){
+              driveBase.driveCartesian(0.0, 0.0, 0.25, 0.0);
+            }else{
+              driveBase.driveCartesian(0.0, 0.0, 0.5, 0.0);
+            }
+        }else if(limelight.tx <= -1){
+            if(limelight.tx > -10){
+              driveBase.driveCartesian(0.0, 0.0, -0.25, 0.0);
+            }else{
+              driveBase.driveCartesian(0.0, 0.0, -0.5, 0.0);
+            }
+        }else{
+            end(true);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
