e7adf5117a3d87bbcdcdb3f7e5652b977159370c
bakedPotatoLord <918780@dpsk12.net>
2022-05-31 10:38:56 -0600

    initial commit

---
+++ src/main/java/frc/robot/commands/StartYeeter.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class StartYeeter extends WaitCommand{
+
+    public StartYeeter(){
+        super(2);
+    }
+
+    public void execute(){
+        
+    }
+
+    public void end(){
+
+    }
+    
+}
