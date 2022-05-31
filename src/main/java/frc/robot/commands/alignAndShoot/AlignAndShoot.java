79c235009947db4684ba099ed64ae83c46806648
BakedPotatoLord <jeffy9745@gmail.com>
2022-05-31 13:25:12 -0600

    finished command

--- src/main/java/frc/robot/commands/alignAndShoot/AlignAndShoot.java
+++ src/main/java/frc/robot/commands/alignAndShoot/AlignAndShoot.java
@@ -1,10 +1,12 @@
 package frc.robot.commands.alignAndShoot;
 
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Yeeter;
 
 
 public class AlignAndShoot extends SequentialCommandGroup {
@@ -13,20 +15,25 @@ public class AlignAndShoot extends SequentialCommandGroup {
     private DriveBase drivebase;
     private Intake intake;
     private Limelight limelight;
+    private Yeeter shooter;
 
     
 
-    public AlignAndShoot(DriveBase base,Intake ntake,Limelight light){
+    public AlignAndShoot(DriveBase base,Intake ntake,Limelight light,Yeeter shootr){
 
         drivebase = base;
         intake = ntake;
         limelight = light;
+        shooter = shootr;
 
-        addRequirements(drivebase,intake,limelight);
+        addRequirements(drivebase,intake,limelight,shooter);
 
         addCommands(
             new AlignBase(drivebase, limelight),
-            new SwallowBalls(intake)
+            new SwallowBalls(intake),
+            new InstantCommand(
+                ()->shooter.stopAll()
+            ,shooter)
         );
     }
     
