9cb74bb5d4448a6744e1c3b087b159fb4532ea23
BakedPotatoLord <jeffy9745@gmail.com>
2022-05-31 13:15:04 -0600

    created intakeballs command group

---
+++ src/main/java/frc/robot/commands/IntakeBalls/IntakeBalls.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.IntakeBalls;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LimitSwitch;
+import frc.robot.subsystems.Yeeter;
+
+public class IntakeBalls extends SequentialCommandGroup {
+
+    private static Intake intake;
+    private LimitSwitch lSwitch;
+    private Yeeter shooter;
+
+    public IntakeBalls(Intake ntake,LimitSwitch limitswitch,Yeeter shootr){
+
+        intake = ntake;
+        lSwitch = limitswitch;
+        shooter = shootr;
+        addRequirements(intake,lSwitch,shootr);
+
+        addCommands(
+            new StartIntake(intake, lSwitch),
+            new StartShooter(shooter)
+        );
+    }
+}
