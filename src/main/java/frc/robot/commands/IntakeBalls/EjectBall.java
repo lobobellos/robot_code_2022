5255ddc531d141fe63ef5de2d2f1749524d359d5
bakedPotatoLord <918780@dpsk12.net>
2022-05-31 14:21:19 -0600

    created ejectball class

---
+++ src/main/java/frc/robot/commands/IntakeBalls/EjectBall.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.IntakeBalls;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Intake;
+
+public class EjectBall extends WaitCommand {
+
+    private Intake intake;
+
+    public EjectBall(Intake ntake){
+        super(0.5);
+
+        intake = ntake;
+
+        addRequirements(intake);
+    }
+
+
+    public void initialize(){
+        intake.setVoltage(-8);
+    }
+
+    public void end(boolean i){
+        intake.stop();
+    }
+    
+}
