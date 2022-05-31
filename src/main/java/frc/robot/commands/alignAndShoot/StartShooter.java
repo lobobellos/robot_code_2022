f72396c0211ae593933d91bf73ccf8f27b6bd0d6
BakedPotatoLord <jeffy9745@gmail.com>
2022-05-31 11:36:11 -0600

    created startsshooter class

---
+++ src/main/java/frc/robot/commands/alignAndShoot/StartShooter.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.alignAndShoot;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Yeeter;
+
+public class StartShooter extends WaitCommand{
+
+    private Yeeter yeeter;
+
+
+    public StartShooter(Yeeter shooter) {
+        super(2);
+
+        yeeter = shooter;
+
+        addRequirements(yeeter);
+    }
+
+    
+
+
+
+    public void initialize(){
+
+        yeeter.setVoltageM(8);
+        yeeter.setVoltageT(8);
+    }
+
+
+
+    public void end(){
+
+    }
+    
+}
