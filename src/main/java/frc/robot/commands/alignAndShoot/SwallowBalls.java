eefc80a8e6804e06b74f0995eb75db78b1ef35bd
bakedPotatoLord <918780@dpsk12.net>
2022-06-01 10:23:08 -0600

    finalized and made fancy

--- src/main/java/frc/robot/commands/alignAndShoot/SwallowBalls.java
+++ src/main/java/frc/robot/commands/alignAndShoot/SwallowBalls.java
@@ -2,23 +2,31 @@ package frc.robot.commands.alignAndShoot;
 
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Yeeter;
 
 public class SwallowBalls extends WaitCommand {
 
     private Intake intake;
+    private Yeeter shooter;
 
-    public SwallowBalls(Intake ntake){
+    public SwallowBalls(Intake ntake, Yeeter shootr){
         super(2);
 
         intake = ntake;
-        addRequirements(intake);
+        shooter = shootr;
+        addRequirements(intake, shooter);
     }
 
     public void initialize(){
+        super.initialize();
+        shooter.setVoltageM(8);
+        shooter.setVoltageT(8);
         intake.setVoltage(8);
+        
     }
     
     public void end(){
         intake.stop();
+        shooter.stopAll();
     }
 }
