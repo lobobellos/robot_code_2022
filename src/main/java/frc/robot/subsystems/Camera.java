01add6180f550061d41cf5be3d24d1481fed1023
BakedPotatoLord <918780@dpsk12.net>
2022-06-15 10:15:25 -0600

    fixed indentation

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -4,10 +4,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Camera extends SubsystemBase{
-
     public Camera(){
         CameraServer.startAutomaticCapture();
     }
-
-    
 }
