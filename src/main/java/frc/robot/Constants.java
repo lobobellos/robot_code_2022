3d56f5bff77bb143fcb4e14817ac3d97be8b64b0
BakedPotatoLord <918780@dpsk12.net>
2022-06-15 10:35:32 -0600

    removed unused constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,13 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Ultrasonic;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -19,7 +10,7 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
  */
 public final class Constants {
 
-      //PWM channels
+  //PWM channels
   public static final int kFrontLeftChannel = 2;
   public static final int kRearLeftChannel = 3;
   public static final int kFrontRightChannel = 1;
@@ -33,68 +24,11 @@ public final class Constants {
   public static final int ultrasonicOutputChannel = 0;
   public static final int ultrasonicInputChannel = 1;
 
-  public static double stickX = 0.0;
-	public static double stickY = 0;
-	public static double stickZ = 0;
-  public static double gyroAngle = 0;
-  public static double throttle;
-  public static final double spinTime = 5;
-
-
+  //joystick channels
   public static final int stickChannel = 0;
   public static final int eStopChannel = 1;
-  public static final boolean useGyro = true;
-
-  //vars used for toggling safemode
-  public static boolean safeMode = false;
-  public static boolean safeModeToggle = true;
-
-  //var used for toggling intake
-  public static boolean intakeRunning = false;
-  public double shootSpeed = 6;
-
-  //var used for toggling targeting
-  public static boolean targetingRunning = false;
-
-  //vars used for targeting shooter
-  public static boolean shooterRunning = false;
-  public boolean limeLightAlign= false;
-
-  
-  public static final double deadZoneX = 0.1;
-	public static final double deadZoneY = 0.1;
-	public static final double deadZoneZ = 0.5;
-
-
-  public Ultrasonic uSonic;
-  public DigitalInput limitSwitch;
-
-  public Spark m_intakeL;
-  public Spark m_shooterM;
-  public Spark m_shooterT;
 
   //estopped
   public boolean eStopped;
-
-  //timers used for launching 
-  public Timer shooterClock;
-  public Timer switchIntakeTimer;
-
-  //bools used for targeting and launching
-  public boolean hasBall = false;
-  public boolean secondaryMovement = false;
-  public boolean runTargeting = false;
-  public boolean runIntake = false;
-  public boolean sonicAlign = false;
-
-
-  //Climbing mechanism
-  public Spark m_climb;
-  public boolean climbRunning = false;
-
-  //Allignment mechanism
-  public boolean toggleAllign = false;
-  public boolean distanceStage = false;
-  public boolean allignStage = false;
 
 }
